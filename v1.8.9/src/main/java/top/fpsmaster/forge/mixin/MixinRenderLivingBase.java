8ca5749a9b5203926f981617bedc541393209732
SuperSkidder <SuperSkidder@outlook.com>
2024-06-21 00:15:30 +0800

    优化leveltag和hitcolor mixin注入的方式

--- v1.8.9/src/main/java/top/fpsmaster/forge/mixin/MixinRenderLivingBase.java
+++ v1.8.9/src/main/java/top/fpsmaster/forge/mixin/MixinRenderLivingBase.java
@@ -28,30 +28,7 @@ public abstract class MixinRenderLivingBase {
     public void tag(EntityLivingBase entity, CallbackInfoReturnable<Boolean> cir) {
         if (LevelTag.using && LevelTag.showSelf.getValue()) {
             cir.setReturnValue(true);
-            return;
         }
-        EntityPlayerSP entityplayersp = Minecraft.getMinecraft().thePlayer;
-        boolean flag = !entity.isInvisibleToPlayer(entityplayersp);
-        if (entity != entityplayersp) {
-            Team team = entity.getTeam();
-            Team team1 = entityplayersp.getTeam();
-            if (team != null) {
-                Team.EnumVisible team$enumvisible = team.getNameTagVisibility();
-                switch (team$enumvisible) {
-                    case ALWAYS:
-                        cir.setReturnValue(flag);
-                    case NEVER:
-                        cir.setReturnValue(false);
-                    case HIDE_FOR_OTHER_TEAMS:
-                        cir.setReturnValue(team1 == null ? flag : team.isSameTeam(team1) && (team.getSeeFriendlyInvisiblesEnabled() || flag));
-                    case HIDE_FOR_OWN_TEAM:
-                        cir.setReturnValue(team1 == null ? flag : !team.isSameTeam(team1) && flag);
-                    default:
-                        cir.setReturnValue(true);
-                }
-            }
-        }
-        cir.setReturnValue(Minecraft.isGuiEnabled() && entity != Minecraft.getMinecraft().getRenderManager().livingPlayer && !entity.isInvisibleToPlayer(entityplayersp) && entity.riddenByEntity == null);
     }
 
     @Shadow
@@ -64,85 +41,83 @@ public abstract class MixinRenderLivingBase {
     @Final
     private static DynamicTexture textureBrightness;
 
-    /**
-     * @author SuperSkidder
-     * @reason hitcolor;
-     */
-    @Overwrite
-    protected boolean setBrightness(EntityLivingBase entitylivingbaseIn, float partialTicks, boolean combineTextures) {
-        float f = entitylivingbaseIn.getBrightness(partialTicks);
-        int i = this.getColorMultiplier(entitylivingbaseIn, f, partialTicks);
-        boolean flag = (i >> 24 & 255) > 0;
-        boolean flag1 = entitylivingbaseIn.hurtTime > 0 || entitylivingbaseIn.deathTime > 0;
-        if (!flag && !flag1) {
-            return false;
-        } else if (!flag && !combineTextures) {
-            return false;
-        } else {
-            GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
-            GlStateManager.enableTexture2D();
-            GL11.glTexEnvi(8960, 8704, OpenGlHelper.GL_COMBINE);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_RGB, 8448);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_RGB, OpenGlHelper.defaultTexUnit);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE1_RGB, OpenGlHelper.GL_PRIMARY_COLOR);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_RGB, 768);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND1_RGB, 768);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_ALPHA, 7681);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_ALPHA, OpenGlHelper.defaultTexUnit);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_ALPHA, 770);
-            GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
-            GlStateManager.enableTexture2D();
-            GL11.glTexEnvi(8960, 8704, OpenGlHelper.GL_COMBINE);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_RGB, OpenGlHelper.GL_INTERPOLATE);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_RGB, OpenGlHelper.GL_CONSTANT);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE1_RGB, OpenGlHelper.GL_PREVIOUS);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE2_RGB, OpenGlHelper.GL_CONSTANT);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_RGB, 768);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND1_RGB, 768);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND2_RGB, 770);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_ALPHA, 7681);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_ALPHA, OpenGlHelper.GL_PREVIOUS);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_ALPHA, 770);
-            this.brightnessBuffer.position(0);
-            if (flag1) {
-                if (HitColor.using) {
-                    this.brightnessBuffer.put(HitColor.getRed());
-                    this.brightnessBuffer.put(HitColor.getGreen());
-                    this.brightnessBuffer.put(HitColor.getBlue());
-                    this.brightnessBuffer.put(HitColor.getAlpha());
-                } else {
-                    this.brightnessBuffer.put(1.0F);
-                    this.brightnessBuffer.put(0.0F);
-                    this.brightnessBuffer.put(0.0F);
-                    this.brightnessBuffer.put(0.3F);
-                }
+    @Inject(method = "setBrightness", at = @At("HEAD"), cancellable = true)
+    protected void setBrightness(EntityLivingBase entitylivingbaseIn, float partialTicks, boolean combineTextures, CallbackInfoReturnable<Boolean> cir) {
+        if (HitColor.using) {
+            float f = entitylivingbaseIn.getBrightness(partialTicks);
+            int i = this.getColorMultiplier(entitylivingbaseIn, f, partialTicks);
+            boolean flag = (i >> 24 & 255) > 0;
+            boolean flag1 = entitylivingbaseIn.hurtTime > 0 || entitylivingbaseIn.deathTime > 0;
+            if (!flag && !flag1) {
+                cir.setReturnValue(false);
+            } else if (!flag && !combineTextures) {
+                cir.setReturnValue(false);
             } else {
-                float f1 = (float)(i >> 24 & 255) / 255.0F;
-                float f2 = (float)(i >> 16 & 255) / 255.0F;
-                float f3 = (float)(i >> 8 & 255) / 255.0F;
-                float f4 = (float)(i & 255) / 255.0F;
-                this.brightnessBuffer.put(f2);
-                this.brightnessBuffer.put(f3);
-                this.brightnessBuffer.put(f4);
-                this.brightnessBuffer.put(1.0F - f1);
-            }
+                GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
+                GlStateManager.enableTexture2D();
+                GL11.glTexEnvi(8960, 8704, OpenGlHelper.GL_COMBINE);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_RGB, 8448);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_RGB, OpenGlHelper.defaultTexUnit);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE1_RGB, OpenGlHelper.GL_PRIMARY_COLOR);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_RGB, 768);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND1_RGB, 768);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_ALPHA, 7681);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_ALPHA, OpenGlHelper.defaultTexUnit);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_ALPHA, 770);
+                GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+                GlStateManager.enableTexture2D();
+                GL11.glTexEnvi(8960, 8704, OpenGlHelper.GL_COMBINE);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_RGB, OpenGlHelper.GL_INTERPOLATE);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_RGB, OpenGlHelper.GL_CONSTANT);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE1_RGB, OpenGlHelper.GL_PREVIOUS);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE2_RGB, OpenGlHelper.GL_CONSTANT);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_RGB, 768);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND1_RGB, 768);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND2_RGB, 770);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_ALPHA, 7681);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_ALPHA, OpenGlHelper.GL_PREVIOUS);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_ALPHA, 770);
+                this.brightnessBuffer.position(0);
+                if (flag1) {
+                    if (HitColor.using) {
+                        this.brightnessBuffer.put(HitColor.getRed());
+                        this.brightnessBuffer.put(HitColor.getGreen());
+                        this.brightnessBuffer.put(HitColor.getBlue());
+                        this.brightnessBuffer.put(HitColor.getAlpha());
+                    } else {
+                        this.brightnessBuffer.put(1.0F);
+                        this.brightnessBuffer.put(0.0F);
+                        this.brightnessBuffer.put(0.0F);
+                        this.brightnessBuffer.put(0.3F);
+                    }
+                } else {
+                    float f1 = (float) (i >> 24 & 255) / 255.0F;
+                    float f2 = (float) (i >> 16 & 255) / 255.0F;
+                    float f3 = (float) (i >> 8 & 255) / 255.0F;
+                    float f4 = (float) (i & 255) / 255.0F;
+                    this.brightnessBuffer.put(f2);
+                    this.brightnessBuffer.put(f3);
+                    this.brightnessBuffer.put(f4);
+                    this.brightnessBuffer.put(1.0F - f1);
+                }
 
-            this.brightnessBuffer.flip();
-            GL11.glTexEnv(8960, 8705, this.brightnessBuffer);
-            GlStateManager.setActiveTexture(OpenGlHelper.GL_TEXTURE2);
-            GlStateManager.enableTexture2D();
-            GlStateManager.bindTexture(textureBrightness.getGlTextureId());
-            GL11.glTexEnvi(8960, 8704, OpenGlHelper.GL_COMBINE);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_RGB, 8448);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_RGB, OpenGlHelper.GL_PREVIOUS);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE1_RGB, OpenGlHelper.lightmapTexUnit);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_RGB, 768);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND1_RGB, 768);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_ALPHA, 7681);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_ALPHA, OpenGlHelper.GL_PREVIOUS);
-            GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_ALPHA, 770);
-            GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
-            return true;
+                this.brightnessBuffer.flip();
+                GL11.glTexEnv(8960, 8705, this.brightnessBuffer);
+                GlStateManager.setActiveTexture(OpenGlHelper.GL_TEXTURE2);
+                GlStateManager.enableTexture2D();
+                GlStateManager.bindTexture(textureBrightness.getGlTextureId());
+                GL11.glTexEnvi(8960, 8704, OpenGlHelper.GL_COMBINE);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_RGB, 8448);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_RGB, OpenGlHelper.GL_PREVIOUS);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE1_RGB, OpenGlHelper.lightmapTexUnit);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_RGB, 768);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND1_RGB, 768);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_COMBINE_ALPHA, 7681);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_SOURCE0_ALPHA, OpenGlHelper.GL_PREVIOUS);
+                GL11.glTexEnvi(8960, OpenGlHelper.GL_OPERAND0_ALPHA, 770);
+                GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
+                cir.setReturnValue(true);
+            }
         }
     }
 }
