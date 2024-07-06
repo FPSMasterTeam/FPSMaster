19bba47be683a6963f1e51f25fa5fe34a9bc8566
SuperSkidder <SuperSkidder@outlook.com>
2024-07-06 17:59:12 +0800

    删除历史遗留的花雨庭相关代码

--- shared/java/top/fpsmaster/features/manager/ModuleManager.kt
+++ shared/java/top/fpsmaster/features/manager/ModuleManager.kt
@@ -38,16 +38,6 @@ class ModuleManager {
                 module.toggle()
             }
         }
-
-        // 提醒一下lowiq怎么组队
-        if (ProviderManager.mcProvider.getPlayer() != null && ProviderManager.mcProvider.getServerAddress()?.contains("hytpc") == true) {
-            if (e.key == Keyboard.KEY_K) {
-                Utility.sendClientMessage("花雨庭组队提示：")
-                Utility.sendClientMessage("创建队伍并邀请玩家：/组队 玩家名")
-                Utility.sendClientMessage("接受组队邀请：/组队 邀请你的玩家名 【如果接受方是网易版请忽略该步骤】")
-                Utility.sendClientMessage("解散队伍：/组队 解散")
-            }
-        }
     }
 
     fun init() {
--- v1.12.2/src/main/java/top/fpsmaster/forge/mixin/MixinNetHandlerLoginClient.java
+++ v1.12.2/src/main/java/top/fpsmaster/forge/mixin/MixinNetHandlerLoginClient.java
@@ -12,7 +12,6 @@ import net.minecraft.network.login.client.CPacketEncryptionResponse;
 import net.minecraft.network.login.server.SPacketEncryptionRequest;
 import net.minecraft.util.CryptManager;
 import net.minecraft.util.text.TextComponentTranslation;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.*;
 import top.fpsmaster.event.EventDispatcher;
@@ -33,55 +32,8 @@ public abstract class MixinNetHandlerLoginClient {
     @Final
     @Shadow
     private static Logger LOGGER;
-    @Unique
-    private boolean isHyt = false;
 
 
     @Shadow
     protected abstract MinecraftSessionService getSessionService();
-
-
-    /**
-     * @author SuperSkidder
-     * @reason for plugin
-     */
-    @Overwrite
-    public void handleEncryptionRequest(SPacketEncryptionRequest packetIn) {
-        final SecretKey secretkey = CryptManager.createNewSharedKey();
-        String s = packetIn.getServerId();
-        PublicKey publickey = packetIn.getPublicKey();
-        String s1 = (new BigInteger(CryptManager.getServerIdHash(s, publickey, secretkey))).toString(16);
-        EventJoinServer eventJoinServer = new EventJoinServer(s1);
-        EventDispatcher.dispatchEvent(eventJoinServer);
-        isHyt = eventJoinServer.getCancel();
-        System.out.println("isHyt: " + isHyt);
-        if (mc.getCurrentServerData() != null && mc.getCurrentServerData().isOnLAN()) {
-            try {
-                this.getSessionService().joinServer(mc.getSession().getProfile(), mc.getSession().getToken(), s1);
-            } catch (AuthenticationException var10) {
-                LOGGER.warn("Couldn't connect to auth servers but will continue to join LAN");
-            }
-        } else {
-            if (!isHyt) {
-                try {
-                    this.getSessionService().joinServer(mc.getSession().getProfile(), mc.getSession().getToken(), s1);
-                } catch (AuthenticationUnavailableException var7) {
-                    this.networkManager.closeChannel(new TextComponentTranslation("disconnect.loginFailedInfo", new TextComponentTranslation("disconnect.loginFailedInfo.serversUnavailable")));
-                    return;
-                } catch (InvalidCredentialsException var8) {
-                    this.networkManager.closeChannel(new TextComponentTranslation("disconnect.loginFailedInfo", new TextComponentTranslation("disconnect.loginFailedInfo.invalidSession")));
-                    return;
-                } catch (AuthenticationException var9) {
-                    this.networkManager.closeChannel(new TextComponentTranslation("disconnect.loginFailedInfo", var9.getMessage()));
-                    return;
-                }
-            }
-        }
-        isHyt = false;
-        this.networkManager.sendPacket(new CPacketEncryptionResponse(secretkey, publickey, packetIn.getVerifyToken()), new GenericFutureListener<Future<? super Void>>() {
-            public void operationComplete(Future<? super Void> p_operationComplete_1_) throws Exception {
-                networkManager.enableEncryption(secretkey);
-            }
-        });
-    }
 }
